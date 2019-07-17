9ee873984f3771a449837959bb9eefcb190b63cd
Dominik George <dominik.george@teckids.org>
2019-07-17 23:50:01 +0200

    Fix style.

--- biscuit/apps/chronos/views.py
+++ biscuit/apps/chronos/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 
 from biscuit.core.decorators import admin_required
+
 
 @login_required
 @admin_required
 def timetable(request):
     context = {}
 
-    return render(request, 'chronos/tt_week.html')
+    return render(request, 'chronos/tt_week.html', context)
