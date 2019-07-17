f9f0431270398051c5193ab2a172c9b0943cbcef
Tom Teichler <tom.teichler@teckids.org>
2019-07-17 13:17:57 +0200

    Add initial tt_week.html.

--- biscuit/apps/chronos/views.py
+++ biscuit/apps/chronos/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
 
 from biscuit.core.decorators import admin_required
-
 
 @login_required
 @admin_required
 def timetable(request):
-    pass
+    context = {}
+
+    return render(request, 'chronos/tt_week.html')
