a9aaffa42157cddd12bfc92415f77a8fac71ad5d
Dominik George <dominik.george@teckids.org>
2019-08-17 13:21:08 +0200

    Remove forgotten references to cambro.

--- biscuit/apps/chronos/migrations/0001_initial.py
+++ biscuit/apps/chronos/migrations/0001_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('cambro', '0001_initial'),
+        ('core', '0001_initial'),
     ]
 
     operations = [
