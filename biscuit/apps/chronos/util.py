5e8efdcbb326e4b19ed58fd1d1b1e0d995e95882
Dominik George <nik@naturalnet.de>
2019-08-21 22:38:10 +0200

    Implement function to get all currently running lessons.
    Advances BiscuIT-App-Alsijil#1.

--- biscuit/apps/chronos/util.py
+++ biscuit/apps/chronos/util.py
@@ -1,5 +1,17 @@
 from datetime import datetime
 
+from .models import LessonPeriod
+
 
 def current_week():
     return int(datetime.now().strftime('%V'))
+
+
+def current_lesson_periods(when=None):
+    now = when or datetime.now()
+
+    return LessonPeriod.objects.filter(lesson__date_start__lte=now.date(),
+                                       lesson__date_end__gte=now.date(),
+                                       period__weekday=now.isoweekday(),
+                                       period__time_start__lte=now.time(),
+                                       period__time_end__gte=now.time())
