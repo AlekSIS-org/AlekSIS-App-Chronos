0042b69866f6df8d18d16effa3c6a54a040f8927
Dominik George <dominik.george@teckids.org>
2019-08-22 14:52:06 +0200

    Move model import to function.

--- biscuit/apps/chronos/util.py
+++ biscuit/apps/chronos/util.py
@@ -4,8 +4,6 @@ from typing import Optional
 from django.apps import apps
 from django.db import models
 
-LessonPeriod = apps.get_model('chronos.LessonPeriod')  # noqa
-
 
 def current_week() -> int:
     return int(datetime.now().strftime('%V'))
@@ -14,6 +12,7 @@ def current_week() -> int:
 def current_lesson_periods(when: Optional[datetime] = None) -> models.query.QuerySet:
     now = when or datetime.now()
 
+    LessonPeriod = apps.get_model('chronos.LessonPeriod')
     return LessonPeriod.objects.filter(lesson__date_start__lte=now.date(),
                                        lesson__date_end__gte=now.date(),
                                        period__weekday=now.isoweekday(),
