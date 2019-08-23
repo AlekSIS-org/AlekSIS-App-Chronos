7f47f710ebbb57225c0970eee02f8a0f8ca62620
Dominik George <dominik.george@teckids.org>
2019-08-23 18:37:25 +0200

    Add filter selection form for timetables. Advances #4.

--- biscuit/apps/chronos/views.py
+++ biscuit/apps/chronos/views.py
@@ -10,6 +10,7 @@ from django.utils.translation import ugettext as _
 from biscuit.core.decorators import admin_required
 from biscuit.core.models import Group, Person
 
+from .forms import SelectForm
 from .models import LessonPeriod, TimePeriod, Room
 from .util import current_week
 
@@ -20,23 +21,16 @@ def timetable(request: HttpRequest) -> HttpResponse:
     context = {}
 
     lesson_periods = LessonPeriod.objects.all()
-    filter_descs = []
 
     if 'group' in request.GET:
         lesson_periods = lesson_periods.filter(
             lesson__groups__pk=int(request.GET['group']))
-        filter_descs.append(_('Group: %s') % Group.objects.get(
-            pk=int(request.GET['group'])))
     elif 'teacher' in request.GET:
         lesson_periods = lesson_periods.filter(
             lesson__teachers__pk=int(request.GET['teacher']))
-        filter_descs.append(_('Teacher: %s') % Person.objects.get(
-            pk=int(request.GET['teacher'])))
     elif 'room' in request.GET:
         lesson_periods = lesson_periods.filter(
             room__pk=int(request.GET['room']))
-        filter_descs.append(_('Room: %s') % Room.objects.get(
-            pk=int(request.GET['room'])))
     else:
         if request.user.person:
             if request.user.person.primary_group:
@@ -72,10 +66,13 @@ def timetable(request: HttpRequest) -> HttpResponse:
         per_day[weekday_num] = OrderedDict(
             sorted(per_day[weekday_num].items()))
 
+    # Add a form to filter the view
+    select_form = SelectForm(request.GET or None)
+
     context['lesson_periods'] = OrderedDict(sorted(per_day.items()))
-    context['filter_descs'] = ', '.join(filter_descs)
     context['periods'] = TimePeriod.get_times_dict()
     context['weekdays'] = dict(TimePeriod.WEEKDAY_CHOICES)
     context['current_week'] = current_week()
+    context['select_form'] = select_form
 
     return render(request, 'chronos/tt_week.html', context)
