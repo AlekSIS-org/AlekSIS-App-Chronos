d1bd289a52d3974cbad4c015e8837cf9787fa445
Dominik George <nik@naturalnet.de>
2019-08-27 12:55:58 +0200

    Fix week days calculation.

--- biscuit/apps/chronos/util.py
+++ biscuit/apps/chronos/util.py
@@ -16,7 +16,7 @@ def week_days(week: Optional[int]) -> Sequence[date]:
     year = date.today().year
     wanted_week = week or current_week()
 
-    first_day = datetime.strptime('%d-%d-1' % (year, wanted_week), '%Y-%W-%w')
+    first_day = datetime.strptime('%d-%d-0' % (year, wanted_week), '%Y-%V-%u')
 
     return [(first_day + timedelta(days=offset)).date() for offset in range(0, 7)]
 
