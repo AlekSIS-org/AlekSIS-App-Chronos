d804aa9bd6ef75f876ae001f51bb97bc6c65ab2b
Dominik George <dominik.george@teckids.org>
2019-08-31 22:50:50 +0200

    Add row_attr helper to highlight lessons with subsitutions. Advances #19, #12.

--- biscuit/apps/chronos/tables.py
+++ biscuit/apps/chronos/tables.py
@@ -2,6 +2,14 @@ from django.utils.translation import ugettext_lazy as _
 import django_tables2 as tables
 from django_tables2.utils import A
 
+
+def _css_class_from_lesson_state(record: Optional[LessonPeriod] = None, table: Optional[LessonTable] = None) -> str:
+    if record.get_substitution(table._week):
+        return 'table-warning'
+    else:
+        return ''
+
+
 class LessonsTable(tables.Table):
     class Meta:
         attrs = {'class': 'table table-striped table-bordered table-hover table-responsive-xl'}
@@ -15,3 +23,6 @@ class LessonsTable(tables.Table):
     def __init__(self, week, *args, **kwargs):
         self._week = week
         super().__init__(*args, **kwargs)
+
+    class Meta:
+        row_attrs = {'class': _css_class_from_lesson_state}
