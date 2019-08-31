5525a696676ac8781348ce40f626e6ef920f0b58
Tom Teichler <tom.teichler@teckids.org>
2019-09-01 00:00:15 +0200

    Fix syntax.

--- biscuit/apps/chronos/urls.py
+++ biscuit/apps/chronos/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('timetable', views.timetable, name='timetable'),
     path('lessons', views.lessons_day, name='lessons_day'),
-    path('lessons/<when>', views.lessons_day, name='lessons_day_by_date')
+    path('lessons/<when>', views.lessons_day, name='lessons_day_by_date'),
     path('substitutions/<id>/edit', views.edit_substitution, name='edit_substitution')
 ]
