e6757886f190b431ef2072bb4c0476291ac0d4df
Tom Teichler <tom.teichler@teckids.org>
2019-09-01 00:19:00 +0200

    Move function to right place.

--- biscuit/apps/chronos/tables.py
+++ biscuit/apps/chronos/tables.py
@@ -7,13 +7,6 @@ from django_tables2.utils import A
 from .models import LessonPeriod
 
 
-def _css_class_from_lesson_state(record: Optional[LessonPeriod] = None, table: Optional[LessonTable] = None) -> str:
-    if record.get_substitution(table._week):
-        return 'table-warning'
-    else:
-        return ''
-
-
 class LessonsTable(tables.Table):
     class Meta:
         attrs = {'class': 'table table-striped table-bordered table-hover table-responsive-xl'}
@@ -29,3 +22,10 @@ class LessonsTable(tables.Table):
     def __init__(self, week, *args, **kwargs):
         self._week = week
         super().__init__(*args, **kwargs)
+
+
+def _css_class_from_lesson_state(record: Optional[LessonPeriod] = None, table: Optional[LessonTable] = None) -> str:
+    if record.get_substitution(table._week):
+        return 'table-warning'
+    else:
+        return ''
