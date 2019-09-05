85e77fb9f8a5fd825d16ae29a402ba478597f354
Dominik George <nik@naturalnet.de>
2019-09-05 22:32:52 +0200

    Fix name translation.

--- biscuit/apps/chronos/apps.py
+++ biscuit/apps/chronos/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
-from django.utils.translation import ugettext as _
 
 
 class ChronosConfig(AppConfig):
     name = 'biscuit.apps.chronos'
-    verbose_name = 'BiscuIT - Chronos (' + _('Timetables') + ')'
+    verbose_name = 'BiscuIT - Chronos (Timetables)'
