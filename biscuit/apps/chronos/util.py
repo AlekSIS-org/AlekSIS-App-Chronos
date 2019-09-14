1d9688684283aa0679831cbe5941127582474946
Dominik George <dominik.george@teckids.org>
2019-09-14 17:54:24 +0200

    Fix typo.

--- biscuit/apps/chronos/util.py
+++ biscuit/apps/chronos/util.py
@@ -28,7 +28,7 @@ class CalendarWeek:
         if not self.year:
             self.year = today.year
         if not self.week:
-            self.week = int(today.strptime('%V'))
+            self.week = int(today.strftime('%V'))
 
     def __str__(self) -> str:
         return '%s %d (%s %s %s)' % (_('Kalenderwoche'), self.week, self[0], _('to'), self[-1])
