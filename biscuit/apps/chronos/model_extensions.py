804350a646291e95e271366d3e83e32479839ac7
Dominik George <dominik.george@teckids.org>
2019-11-07 22:11:24 +0100

    Add model extensions for a person's lesson periods.

--- biscuit/apps/chronos/model_extensions.py
+++ biscuit/apps/chronos/model_extensions.py
@@ -1,8 +1,18 @@
 from biscuit.core.models import Person
 
-from .models import Lesson
+from .models import Lesson, LessonPeriod
 
 
 @Person.property
 def lessons_as_participant(self):
     return Lesson.objects.filter(groups__members=self)
+
+
+@Person.property
+def lesson_periods_as_participant(self):
+    return LessonPeriod.objects.filter(lesson__groups__members=self)
+
+
+@Person.property
+def lesson_periods_as_teacher(self):
+    return LessonPeriod.objects.filter(lesson__teachers=self)
