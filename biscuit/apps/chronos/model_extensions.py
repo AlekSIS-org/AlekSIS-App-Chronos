1b2f90e250bcd732511f2f355296611549731e1f
Dominik George <nik@naturalnet.de>
2019-11-11 12:02:09 +0100

    Document injected methods.

--- biscuit/apps/chronos/model_extensions.py
+++ biscuit/apps/chronos/model_extensions.py
@@ -5,14 +5,44 @@ from .models import Lesson, LessonPeriod
 
 @Person.property
 def lessons_as_participant(self):
+    """ Return a `QuerySet` containing all `Lesson`s this person
+    participates in (as student).
+
+    .. note:: Only available when BiscuIT-App-Chronos is installed.
+
+    :Date: 2019-11-07
+    :Authors:
+        - Dominik George <dominik.george@teckids.org>
+    """
+
     return Lesson.objects.filter(groups__members=self)
 
 
 @Person.property
 def lesson_periods_as_participant(self):
+    """ Return a `QuerySet` containing all `LessonPeriod`s this person
+    participates in (as student).
+
+    .. note:: Only available when BiscuIT-App-Chronos is installed.
+
+    :Date: 2019-11-07
+    :Authors:
+        - Dominik George <dominik.george@teckids.org>
+    """
+
     return LessonPeriod.objects.filter(lesson__groups__members=self)
 
 
 @Person.property
 def lesson_periods_as_teacher(self):
+    """ Return a `QuerySet` containing all `Lesson`s this person
+    gives (as teacher).
+
+    .. note:: Only available when BiscuIT-App-Chronos is installed.
+
+    :Date: 2019-11-07
+    :Authors:
+        - Dominik George <dominik.george@teckids.org>
+    """
+
     return LessonPeriod.objects.filter(lesson__teachers=self)
