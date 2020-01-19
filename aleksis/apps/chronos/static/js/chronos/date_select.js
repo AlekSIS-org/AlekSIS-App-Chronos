ede9d94526762b596dc2a3f0f1d996b57231e37a
Jonathan Weth <git@jonathanweth.de>
2020-01-19 16:05:47 +0100

    Use django-generated URLs for next/previous buttons in datepicker

--- aleksis/apps/chronos/static/js/chronos/date_select.js
+++ aleksis/apps/chronos/static/js/chronos/date_select.js
@@ -14,31 +14,9 @@ function loadNew() {
     window.location.href = dest + formatDateForDjango(activeDate);
 }
 
-function onDateBeforeClick() {
-    if (activeDate.getDay() === 1) {
-        var minus = 3;
-    } else {
-        var minus = 1;
-    }
-    activeDate.setDate(activeDate.getDate() - minus);
-    update();
-    loadNew();
-}
-
-function onDateNextClick() {
-    if (activeDate.getDay() === 5) {
-        var plus = 3;
-    } else {
-        var plus = 1;
-    }
-    activeDate.setDate(activeDate.getDate() + plus);
-    update();
-    loadNew();
-}
-
 function onDateChanged() {
     var str = $("#date").val();
-    var split = str.split(".")
+    var split = str.split(".");
     activeDate = new Date(split[2], split[1] - 1, split[0]);
     update();
     loadNew();
@@ -46,8 +24,6 @@ function onDateChanged() {
 
 
 $(document).ready(function () {
-    $("#date-before").click(onDateBeforeClick);
-    $("#date-next").click(onDateNextClick);
     $("#date").change(onDateChanged);
 
     update();
