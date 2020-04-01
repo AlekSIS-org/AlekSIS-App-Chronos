526c53fbf85f3d86749aff19755c002ad59b131a
Jonathan Weth <git@jonathanweth.de>
2020-04-01 16:49:40 +0200

    Add support for showing parent groups instead of original groups if there is only one group and this group has parent groups
    Advance #71

--- aleksis/apps/chronos/settings.py
+++ aleksis/apps/chronos/settings.py
@@ -1,6 +1,14 @@
 from django.utils.translation import gettext_lazy as _
 
 CONSTANCE_CONFIG = {
+    "CHRONOS_USE_PARENT_GROUPS": (
+        False,
+        _(
+            "If an lesson or substitution has only one group"
+            " and this group has parent groups,"
+            " show the parent groups instead of the original group."
+        ),
+    ),
     "CHRONOS_SUBSTITUTIONS_PRINT_DAY_NUMBER": (
         2,
         _("Number of days shown on substitutions print view"),
@@ -12,6 +20,7 @@ CONSTANCE_CONFIG = {
 }
 CONSTANCE_CONFIG_FIELDSETS = {
     "Chronos settings": (
+        "CHRONOS_USE_PARENT_GROUPS",
         "CHRONOS_SUBSTITUTIONS_PRINT_DAY_NUMBER",
         "CHRONOS_SUBSTITUTIONS_SHOW_HEADER_BOX",
     ),
