65b33e580bffabe890ccd1c65209b6a2dfc73edf
Jonathan Weth <git@jonathanweth.de>
2020-05-03 16:09:54 +0200

    Fix broken import

--- aleksis/apps/chronos/util/format.py
+++ aleksis/apps/chronos/util/format.py
@@ -1,6 +1,5 @@
 from datetime import date
 
-from aleksis.apps.chronos.models import TimePeriod
 from django.utils.formats import date_format
 
 
@@ -9,6 +8,6 @@ def format_m2m(f, attr: str = "short_name") -> str:
     return ", ".join([getattr(x, attr) for x in f.all()])
 
 
-def format_date_period(day: date, period: TimePeriod) -> str:
+def format_date_period(day: date, period: "TimePeriod") -> str:
     """Format date and time period."""
     return "{}, {}.".format(date_format(day), period.period)
