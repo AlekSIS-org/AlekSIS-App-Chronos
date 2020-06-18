38d200f66b8c8603a1b3be7be22875430a6f4312
Jonathan Weth <git@jonathanweth.de>
2020-06-18 22:13:04 +0200

    Fix usage of SchoolTerm (replace old syntax)

--- aleksis/apps/chronos/models.py
+++ aleksis/apps/chronos/models.py
@@ -36,7 +36,7 @@ from aleksis.apps.chronos.managers import (
 )
 from aleksis.apps.chronos.util.format import format_m2m
 from aleksis.core.mixins import ExtensibleModel
-from aleksis.core.models import DashboardWidget
+from aleksis.core.models import DashboardWidget, SchoolTerm
 from aleksis.core.util.core_helpers import has_person
 
 
@@ -68,7 +68,7 @@ class TimePeriod(ExtensibleModel):
             year = date.today().year
             week_number = week or getattr(self, "_week", None) or CalendarWeek().week
 
-            if week_number < self.school.current_term.date_start.isocalendar()[1]:
+            if week_number < SchoolTerm.current_term.date_start.isocalendar()[1]:
                 year += 1
 
             wanted_week = CalendarWeek(year=year, week=week_number)
